1e4fc8a5a4da181ae4737625c6a17baf0fdf89c5
alexbraz <alexjavabraz@gmail.com>
2019-03-01 18:17:41 -0300

    [namespace] Adding new DTOs, according swagger

---
+++ src/main/java/io/nem/sdk/model/namespace/NamespaceIds.java
@@ -0,0 +1,107 @@
+/*
+ * Copyright 2019 NEM
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package io.nem.sdk.model.namespace;
+
+import java.util.Objects;
+import com.google.gson.TypeAdapter;
+import com.google.gson.annotations.JsonAdapter;
+import com.google.gson.annotations.SerializedName;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonWriter;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * NamespaceIds
+ */
+@javax.annotation.Generated(value = "io.swagger.codegen.languages.JavaClientCodegen", date = "2019-02-28T19:50:06.335-03:00")
+public class NamespaceIds {
+  @SerializedName("namespaceIds")
+  private List<String> namespaceIds = null;
+
+  public NamespaceIds namespaceIds(List<String> namespaceIds) {
+    this.namespaceIds = namespaceIds;
+    return this;
+  }
+
+  public NamespaceIds addNamespaceIdsItem(String namespaceIdsItem) {
+    if (this.namespaceIds == null) {
+      this.namespaceIds = new ArrayList<String>();
+    }
+    this.namespaceIds.add(namespaceIdsItem);
+    return this;
+  }
+
+   /**
+   * Get namespaceIds
+   * @return namespaceIds
+  **/
+  @ApiModelProperty(example = "[\"84b3552d375ffa4b\"]", value = "")
+  public List<String> getNamespaceIds() {
+    return namespaceIds;
+  }
+
+  public void setNamespaceIds(List<String> namespaceIds) {
+    this.namespaceIds = namespaceIds;
+  }
+
+
+  @Override
+  public boolean equals(java.lang.Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    NamespaceIds namespaceIds = (NamespaceIds) o;
+    return Objects.equals(this.namespaceIds, namespaceIds.namespaceIds);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(namespaceIds);
+  }
+
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class NamespaceIds {\n");
+    
+    sb.append("    namespaceIds: ").append(toIndentedString(namespaceIds)).append("\n");
+    sb.append("}");
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces
+   * (except the first line).
+   */
+  private String toIndentedString(java.lang.Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+
+}
+
---
+++ src/main/java/io/nem/sdk/model/namespace/NamespaceInfoDTO.java
@@ -0,0 +1,115 @@
+/*
+ * Copyright 2019 NEM
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package io.nem.sdk.model.namespace;
+
+import com.google.gson.annotations.SerializedName;
+import io.swagger.annotations.ApiModelProperty;
+
+import java.util.Objects;
+
+/**
+ * NamespaceInfoDTO
+ */
+@javax.annotation.Generated(value = "io.swagger.codegen.languages.JavaClientCodegen", date = "2019-02-28T19:50:06.335-03:00")
+public class NamespaceInfoDTO {
+  @SerializedName("meta")
+  private NamespaceMosaicMetaDTO meta = null;
+
+  @SerializedName("namespace")
+  private NamespaceDTO namespace = null;
+
+  public NamespaceInfoDTO meta(NamespaceMosaicMetaDTO meta) {
+    this.meta = meta;
+    return this;
+  }
+
+   /**
+   * Get meta
+   * @return meta
+  **/
+  @ApiModelProperty(value = "")
+  public NamespaceMosaicMetaDTO getMeta() {
+    return meta;
+  }
+
+  public void setMeta(NamespaceMosaicMetaDTO meta) {
+    this.meta = meta;
+  }
+
+  public NamespaceInfoDTO namespace(NamespaceDTO namespace) {
+    this.namespace = namespace;
+    return this;
+  }
+
+   /**
+   * Get namespace
+   * @return namespace
+  **/
+  @ApiModelProperty(required = true, value = "")
+  public NamespaceDTO getNamespace() {
+    return namespace;
+  }
+
+  public void setNamespace(NamespaceDTO namespace) {
+    this.namespace = namespace;
+  }
+
+
+  @Override
+  public boolean equals(java.lang.Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    NamespaceInfoDTO namespaceInfoDTO = (NamespaceInfoDTO) o;
+    return Objects.equals(this.meta, namespaceInfoDTO.meta) &&
+        Objects.equals(this.namespace, namespaceInfoDTO.namespace);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(meta, namespace);
+  }
+
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class NamespaceInfoDTO {\n");
+    
+    sb.append("    meta: ").append(toIndentedString(meta)).append("\n");
+    sb.append("    namespace: ").append(toIndentedString(namespace)).append("\n");
+    sb.append("}");
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces
+   * (except the first line).
+   */
+  private String toIndentedString(java.lang.Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+
+}
+
