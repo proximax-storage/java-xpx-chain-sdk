92e55605816b60d377187f30180f43eddf4106ab
Anton Wiedermann <anton.wiedermann@gmail.com>
2019-05-07 21:21:45 +0200

    added test to create mosaic
    failing for unknown reason now

---
+++ src/integration-test/java/io/proximax/sdk/E2EMosaicTest.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2019 ProximaX
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.proximax.sdk;
+
+import java.math.BigInteger;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import io.proximax.sdk.model.mosaic.MosaicId;
+import io.proximax.sdk.model.mosaic.MosaicProperties;
+import io.proximax.sdk.model.transaction.MosaicDefinitionTransaction;
+import io.proximax.sdk.model.transaction.SignedTransaction;
+
+/**
+ * E2E tests that demonstrate transfers
+ * 
+ * @author tonowie
+ */
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class E2EMosaicTest extends E2EBaseTest {
+   /** logger */
+   private static final Logger logger = LoggerFactory.getLogger(E2EMosaicTest.class);
+
+   @BeforeAll
+   void addListener() {
+   }
+
+   @AfterAll
+   void closeDown() {
+   }
+
+   @Test
+   void createMosaic() {
+      MosaicDefinitionTransaction mdt = MosaicDefinitionTransaction.create(new MosaicId(1, seedAccount.getPublicKey()),
+            getDeadline(),
+            new MosaicProperties(false, true, false, 6, BigInteger.valueOf(20)),
+            NETWORK_TYPE);
+      SignedTransaction signedMosaic = seedAccount.sign(mdt);
+      transactionHttp.announce(signedMosaic).blockingFirst();
+      logger.info("Mosaic created. {}",
+            listener.confirmed(seedAccount.getAddress()).timeout(30, TimeUnit.SECONDS).blockingFirst());
+   }
+
+}
