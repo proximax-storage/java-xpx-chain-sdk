16d586d85450b28458be00eea84d6eb19107c972
Anton Wiedermann <anton.wiedermann@gmail.com>
2019-05-27 14:14:36 +0200

    added "test" to print out standard aliases

--- src/e2e/java/io/proximax/sdk/E2EAliasTest.java
+++ src/e2e/java/io/proximax/sdk/E2EAliasTest.java
@@ -32,10 +32,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import io.proximax.core.crypto.KeyPair;
-import io.proximax.sdk.gen.model.UInt64DTO;
 import io.proximax.sdk.model.account.Account;
 import io.proximax.sdk.model.alias.AliasAction;
 import io.proximax.sdk.model.mosaic.MosaicId;
+import io.proximax.sdk.model.mosaic.MosaicInfo;
 import io.proximax.sdk.model.mosaic.MosaicNames;
 import io.proximax.sdk.model.mosaic.MosaicNonce;
 import io.proximax.sdk.model.mosaic.MosaicProperties;
@@ -45,7 +45,6 @@ import io.proximax.sdk.model.transaction.MosaicDefinitionTransaction;
 import io.proximax.sdk.model.transaction.RegisterNamespaceTransaction;
 import io.proximax.sdk.model.transaction.SignedTransaction;
 import io.proximax.sdk.model.transaction.Transaction;
-import io.proximax.sdk.utils.dto.UInt64Utils;
 import io.reactivex.Observable;
 
 /**
@@ -76,11 +75,6 @@ public class E2EAliasTest extends E2EBaseTest {
       mosaicNonce = MosaicNonce.createRandom();
       mosaicId = new MosaicId(mosaicNonce, seedAccount.getPublicKey());
       signup(seedAccount.getAddress());
-      UInt64DTO dto = new UInt64DTO();
-      dto.add(244048598l);
-      dto.add(73533943l);
-      System.out.println(new MosaicId(UInt64Utils.toBigInt(dto)).getIdAsHex());
-//      fail();
    }
 
    @AfterAll
@@ -88,6 +82,26 @@ public class E2EAliasTest extends E2EBaseTest {
 
    }
 
+   @Test
+   void test00GetMosaicsByAlias() {
+      // try to retrieve standard aliases and print them out
+      logAliasInfo(new NamespaceId("cat.currency"));
+      logAliasInfo(new NamespaceId("prx.xpx"));
+   }
+   
+   private void logAliasInfo(NamespaceId namespace) {
+      try {
+         MosaicInfo catCurrency = mosaicHttp.getMosaic(namespace).blockingFirst();
+         logger.info("{} => {}", namespace, catCurrency);
+      } catch (RuntimeException e) {
+         if (!"Not Found".equals(e.getMessage())) {
+            throw e;
+         } else {
+            logger.info("Alias by {} not found", namespace);
+         }
+      }
+   }
+   
    @Test
    void test01PrepareData() {
       NamespaceId rootId = new NamespaceId(ROOT_NAME);
