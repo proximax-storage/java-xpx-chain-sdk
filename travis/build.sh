c40773565a182ba7a921436e804682f178df8e11
Anton Wiedermann <anton.wiedermann@gmail.com>
2019-06-19 16:35:20 +0200

    removing clean to take advantage of the auto-executed assemble task

--- travis/build.sh
+++ travis/build.sh
@@ -4,8 +4,8 @@ if [ "$TRAVIS_PULL_REQUEST" != "false" ];
 then
     echo "Building pull request - skip e2e test?"
 #    ./gradlew clean build -x integrationTest
-    ./gradlew clean build
+    ./gradlew build
 else
     echo "building branch $TRAVIS_BRANCH"
-    ./gradlew clean build
+    ./gradlew build
 fi
