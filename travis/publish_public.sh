99ac71eadb9446aba9f5f9f28c8965288534fb1c
Anton Wiedermann <anton.wiedermann@gmail.com>
2019-06-20 22:13:08 +0200

    issue #71 more changes to get publishing work on the CI (#77)
    * fixed gradle.properties
    
    * restructured travis build and added decoding of secret key from base64
    
    * fix for the base64 decode
    
    * another fix for the decode
    
    * another attempt to fix the decode
    
    * another attempt for fix
    
    * publish snapshot from pull request to test the functionality
    
    * testing the snapshot publication
    
    * restored functionality to make sure publishing is done when expected

--- travis/publish_public.sh
+++ travis/publish_public.sh
@@ -1,16 +1,25 @@
 #!/usr/bin/env sh
 
+# signing key needs to be decoded from base64 to PGP armored text format
+export ORG_GRADLE_PROJECT_signingKey="`echo $GPG_SECRET_KEYS | base64 -d`"
+# password is used as is
+export ORG_GRADLE_PROJECT_signingPassword=$GPG_PASSPHRASE
+
+# sonatype publishing credentials
 export ORG_GRADLE_PROJECT_sonatypeUsername=$SONATYPE_USERNAME
 export ORG_GRADLE_PROJECT_sonatypePassword=$SONATYPE_PASSWORD
 
+# publish snapshot (using version in gradle.properties)
 if [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ];
 then
 	echo "publishing snapshot to public repository"
 	./gradlew publishAllPublicationsToSnapshotRepository
 fi
 
+# from tag publish release to maven central using tag name as version
 if [ -n "$TRAVIS_TAG" ];
 then 
 	echo "publishing release $TRAVIS_TAG to public repository"
+	# NOTE that version is overriden here by the tag name
 	./gradlew publishAllPublicationsToReleaseRepository -Pversion=$TRAVIS_TAG
 fi
