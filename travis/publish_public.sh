deeb4e4d94df47614a1cd2d81ec7f85f60f8940e
Anton Wiedermann <anton.wiedermann@gmail.com>
2019-06-20 18:03:31 +0200

    another fix for the decode

--- travis/publish_public.sh
+++ travis/publish_public.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 # signing key needs to be decoded from base64 to PGP armored text format
-export ORG_GRADLE_PROJECT_signingKey=`echo $GPG_SECRET_KEYS | base64 -d`
+export ORG_GRADLE_PROJECT_signingKey=$(echo $GPG_SECRET_KEYS | base64 -d)
 # password is used as is
 export ORG_GRADLE_PROJECT_signingPassword=$GPG_PASSPHRASE
 
