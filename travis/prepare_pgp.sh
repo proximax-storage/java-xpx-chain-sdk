403477fcf76d567701545a222dd0453235813dcb
Anton Wiedermann <anton.wiedermann@gmail.com>
2019-06-20 10:42:43 +0200

    issue #71 - implemented publishing of snapshots and tag releases

--- travis/prepare_pgp.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-
-if [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ];
-then
-	echo "[NOT IMPLEMENTED] prepare gpg key"
-#    openssl aes-256-cbc -K $encrypted_XXXXXX_key -iv $encrypted_XXXXXX_iv -in $ENCRYPTED_GPG_KEY_LOCATION -out $GPG_KEY_LOCATION -d
-fi
--- travis/publish_public.sh
+++ travis/publish_public.sh
@@ -2,6 +2,12 @@
 
 if [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ];
 then
-	echo "publishing release to public repositories"
-#	./gradlew publishAllPublicationsToMavenCentralRepository
+	echo "publishing snapshot to public repository"
+	./gradlew publishAllPublicationsToSnapshotRepository
+fi
+
+if [ -n "$TRAVIS_TAG" ];
+then 
+	echo "publishing release $TRAVIS_TAG to public repository"
+	./gradlew publishAllPublicationsToReleaseRepository -Pversion=$TRAVIS_TAG
 fi
