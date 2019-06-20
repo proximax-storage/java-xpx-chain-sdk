64005be5de0c9556ed2f57873a5044c22726352c
Anton Wiedermann <anton.wiedermann@gmail.com>
2019-06-20 20:55:18 +0200

    restored functionality to make sure publishing is done when expected

--- travis/publish_public.sh
+++ travis/publish_public.sh
@@ -9,10 +9,6 @@ export ORG_GRADLE_PROJECT_signingPassword=$GPG_PASSPHRASE
 export ORG_GRADLE_PROJECT_sonatypeUsername=$SONATYPE_USERNAME
 export ORG_GRADLE_PROJECT_sonatypePassword=$SONATYPE_PASSWORD
 
-	echo "publishing snapshot to public repository"
-	./gradlew publishAllPublicationsToSnapshotRepository
-
-
 # publish snapshot (using version in gradle.properties)
 if [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ];
 then
