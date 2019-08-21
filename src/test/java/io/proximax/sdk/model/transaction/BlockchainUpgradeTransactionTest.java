b35ab86f759da715f717dec54e51331c692eedbc
Anton Wiedermann <anton.wiedermann@gmail.com>
2019-08-21 18:37:10 +0200

    issue #112 - finished junit test for blockchain upgrade trans

--- src/test/java/io/proximax/sdk/model/transaction/BlockchainUpgradeTransactionTest.java
+++ src/test/java/io/proximax/sdk/model/transaction/BlockchainUpgradeTransactionTest.java
@@ -5,26 +5,40 @@
  */
 package io.proximax.sdk.model.transaction;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.io.IOException;
 import java.math.BigInteger;
 
 import org.junit.jupiter.api.Test;
 
+import io.proximax.sdk.ResourceBasedTest;
 import io.proximax.sdk.model.blockchain.BlockchainVersion;
 import io.proximax.sdk.model.blockchain.NetworkType;
 
 /**
  * {@link BlockchainUpgradeTransaction} tests
  */
-class BlockchainUpgradeTransactionTest {
+class BlockchainUpgradeTransactionTest extends ResourceBasedTest {
 
    @Test
    void testConstructor() {
-      BlockchainUpgradeTransaction trans = BlockchainUpgradeTransaction.create(BigInteger.ONE, new BlockchainVersion(1, 2, 3, 4), new FakeDeadline(), NetworkType.MIJIN);
-      
+      BlockchainUpgradeTransaction trans = BlockchainUpgradeTransaction
+            .create(BigInteger.ONE, new BlockchainVersion(1, 2, 3, 4), new FakeDeadline(), NetworkType.MIJIN);
+
       assertEquals(BigInteger.ONE, trans.getUpgradePeriod());
       assertEquals(new BlockchainVersion(1, 2, 3, 4), trans.getNewVersion());
+   }
+
+   @Test
+   void serialization() throws IOException {
+      BlockchainUpgradeTransaction trans = BlockchainUpgradeTransaction
+            .create(BigInteger.ONE, new BlockchainVersion(1, 2, 3, 4), new FakeDeadline(), NetworkType.MIJIN);
+
+      byte[] actual = trans.generateBytes();
+//      saveBytes("blockchain_upgrade", actual);
+      assertArrayEquals(actual, loadBytes("blockchain_upgrade"));
    }
 
 }
